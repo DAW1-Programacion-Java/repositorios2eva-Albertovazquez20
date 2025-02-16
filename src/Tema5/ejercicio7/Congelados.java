130e905e11794d3b5fd8c6b34f5153c532703ccb
Alberto VÁZQUEZ RANDO <avazquezr09@educarex.es>
2025-02-16 21:36:54 +0100

    ejercicios7

--- src/Tema5/ejercicio7/Congelados.java
+++ src/Tema5/ejercicio7/Congelados.java
@@ -1,9 +1,12 @@
 package Tema5.ejercicio7;
 
-public class Congelados {
+import java.time.LocalDate;
+
+public class Congelados extends Producto {
     private float temperaturacongelacion;
 
-    public Congelados(float temperaturacongelacion) {
+    public Congelados(int id, String descripcion, LocalDate fechacaducidad, int numlote, float temperaturacongelacion) {
+        super(id, descripcion, fechacaducidad, numlote);
         this.temperaturacongelacion = temperaturacongelacion;
     }
 
--- src/Tema5/ejercicio7/Main.java
+++ src/Tema5/ejercicio7/Main.java
@@ -1,13 +1,31 @@
 package Tema5.ejercicio7;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Producto> listaProductos = new ArrayList<>();
-        listaProductos.add(new Producto());
-        listaProductos.add(new Frescos());
-        listaProductos.add(new Refrigerados());
-        listaProductos.add(new Congelados());
+        listaProductos.add(new Producto(1, "enlatado", LocalDate.of(2025, 5, 30), 0001));
+        listaProductos.add(new Frescos(2, "Manzana", LocalDate.of(2024, 5, 20), 1002, LocalDate.of(2024, 4, 15), "España"));
+        listaProductos.add(new Refrigerados(3, "Yogur", LocalDate.of(2024, 6, 10), 1003, "RF-123456"));
+        listaProductos.add(new Congelados(4, "Pescado", LocalDate.of(2025, 3, 15), 1004, -18.0f));
+        listaProductos.add(new Congelados(4, "Higado", LocalDate.of(2025, 3, 15), 1004, -20.0f));
+
+
+        for (Producto p : listaProductos){
+            if(p instanceof Frescos){
+                System.out.println("Fresco = " + p);
+
+            }else if(p instanceof Congelados){
+                System.out.println("Congelado = " + p);
+            }else if (p instanceof Refrigerados){
+                System.out.println("Refirgerado = " + p);
+            }else{
+                System.out.println("Producto = " + p);
+            }
+        }
+
     }
 }
